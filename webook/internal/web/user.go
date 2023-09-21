c54a3f9e8ea9d55e9c1879e3ee32fe173047d68a
HuChengzhen <huchengzhen@gmail.com>
2023-09-21 18:23:23 +0800

    new gorm dir

--- webook/internal/web/user.go
+++ webook/internal/web/user.go
@@ -1,20 +1,73 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/mail"
+)
+
+func isValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
 
 type UserHandler struct {
+	passwordRe *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	const passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+	re := regexp.MustCompile(passwordRegexPattern, 0)
+
+	return &UserHandler{
+		passwordRe: re,
+	}
 }
 
 func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	group := server.Group("/users")
-	group.POST("/signup", u.Signup)
+	group.POST("/signup", u.SignUp)
 	group.POST("/login", u.Login)
 	group.POST("/edit", u.Edit)
 	group.GET("/profile", u.Profile)
 }
 
-func (u *UserHandler) Signup(ctx *gin.Context) {
+func (u *UserHandler) SignUp(ctx *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
 
+	var req SignUpReq
+
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	if !isValidEmail(req.Email) {
+		ctx.String(http.StatusOK, "你的邮箱格式不对")
+		return
+	}
+
+	if req.ConfirmPassword != req.Password {
+		ctx.String(http.StatusOK, "两次输入的密码不一致")
+	}
+
+	ok, err := u.passwordRe.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+
+	if !ok {
+		ctx.String(http.StatusOK, "密码必须大于8位，包含特殊字符")
+	}
+	ctx.String(http.StatusOK, "OK")
+	fmt.Printf("%v", req)
+	return
 }
 
 func (u *UserHandler) Login(ctx *gin.Context) {
--- webook/main.go
+++ webook/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"learn-geektime-basic-go/webook/internal/web"
 )
 
 func main() {
 	server := gin.Default()
-	u := &web.UserHandler{}
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowCredentials = true
+	config.AllowHeaders = []string{"authorization", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"}
+	server.Use(cors.New(config))
+
+	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
 	server.Run(":8080")
 }
